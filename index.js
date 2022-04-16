d82643b4eb01654e2fd67fcd8dfd5e65d617f56a
Illya <sypersys228@gmail.com>
2022-04-16 10:01:16 +0200

    obejcts and prompts

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-const vv = [
-    {d: 5},
-    {d: 5},
-];
-let b = 1;
+let numberOfFilms = +prompt("Сколько фильмов вы посмотрели");
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+let a = prompt("Kakoy posledniy film?");
+let b = prompt("Kakaya ocenka");
+let c = prompt("Kakoy posledniy film?");
+let d = prompt("Kakaya ocenka");
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
